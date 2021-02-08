6b50a3471120cf1b167568ee6d80e69b6186b20c
KalluruS <>
2021-02-08 07:20:31 +0530

    Update shell-scripting/

---
+++ 03-variables.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Variable : If we assign a name to a set of data is called as Variable
+
+# Declare a variable in bash shell
+NAME=DEVOPS
+
+# Usually other scripting languages will have data types, But shell by default will not have any data types.. All the data is a string for shell.
+
+COUNT=2
+
+# Access the variable
+
+echo NAME=$NAME
+
+FILE1=sample.txt
+FILE2=new.txt
+
+#cp $FILE1 $FILE2
+
