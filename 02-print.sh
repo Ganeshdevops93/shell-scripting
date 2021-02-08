54f1c21e5f8a32efc9b549038e21f90e9e5eabd8
KalluruS <>
2021-02-08 07:06:30 +0530

    Update shell-scripting/

--- 02-print.sh
+++ 02-print.sh
@@ -11,7 +11,27 @@ echo Welcome to DevOps
 
 # By using ESC sequences in echo we can print colors
 # \n - New line
+# \t - Tab space
+# \e - New color
 
 ## Note: When we use esc seq, Input to echo command should be given in double quotes (can single quotes also be used, but preferred is double quotes), Also esc seq works when you enable -e option to echo command.
 
 echo -e "Hello,\n\nWelcome to DevOps Training"
+
+echo -e "Hello\t\t\tWorld"
+
+## Syntax for colors
+# echo -e "\e[COL-CODEmMESSAGE"
+
+
+## Following are the COL-CODES
+
+#   Color         COde
+#   Red           31
+#   Green         32
+#   Yellow        33
+#   Blue          34
+#   Magenta       35
+#   Cyan          36
+
+echo -e "\e[31mWARNING!! THere is an error"
