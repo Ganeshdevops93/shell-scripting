90b20d06ec256754d5785d30a7331900be259dcb
KalluruS <>
2021-02-10 06:33:44 +0530

    Update shell-scripting/

--- 07-functions.sh
+++ 07-functions.sh
@@ -5,6 +5,7 @@
 # Way1
 sample() {
   echo Hello from sample function
+  echo a = $a
 }
 
 #Way2
@@ -12,6 +13,9 @@ function sample1() {
   echo Hello from sample1 function
 }
 
+
+## Main Program
 ## Access the function
+a=10
 sample
 sample1
