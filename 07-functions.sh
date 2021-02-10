fa0d8f6b26cba2f2cbed1c06ad65cf8cba61c1e6
KalluruS <>
2021-02-10 06:43:42 +0530

    Update shell-scripting/

--- 07-functions.sh
+++ 07-functions.sh
@@ -4,7 +4,7 @@
 
 # Way1
 sample() {
-  return
+  return 10
   a=200
   echo Hello from sample function
   echo a = $a
@@ -22,6 +22,7 @@ function sample1() {
 ## Access the function
 a=10
 sample xyz
+echo Exit status of sample function = $?
 sample1
 b=100
 echo b = $b
