e06e8824418fbbb2ac8738a11ec61079c1234c95
KalluruS <>
2021-02-11 07:34:18 +0530

    Update shell-scripting/

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -5,7 +5,7 @@ COMPONENT=frontend
 source components/common.sh
 
 Print "Installing Nginx" "yum install nginx -y"
-yum install nginx -y
+yumm install nginx -y
 Stat $?
 
 Print "Starting Nginx Service" "systemctl start nginx"
