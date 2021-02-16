9ecabd2698336e7223c860baa0aff4b2bee2aa85
KalluruS <>
2021-02-16 06:32:34 +0530

    Update shell-scripting/

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -32,7 +32,7 @@ if [ $? -ne 0 ]; then
   ALTER USER 'root'@'localhost' IDENTIFIED BY 'Default_RoboShop*999';
   uninstall plugin validate_password;
   ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';
-  EOF
+EOF
   Stat $?
 fi
 
