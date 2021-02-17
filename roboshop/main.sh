802b10bf7827fa1f6a3cfd8dbe906a5566bbcaef
KalluruS <>
2021-02-17 06:28:02 +0530

    Update shell-scripting/

--- roboshop/main.sh
+++ roboshop/main.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+CHECK() {
+  if [ ! -f components/$1.sh ]; then
+    echo "Given Input does not have a script"
+    exit 1
+  fi
+}
+
 case $1 in
-  cart)       sh components/cart.sh ;;
-  catalogue)  sh components/catalogue.sh ;;
-  user)       sh components/user.sh ;;
-  shipping)   sh components/shipping.sh ;;
-  payment)    sh components/payment.sh ;;
-  mongodb)    sh components/monogdb.sh ;;
-  redis)      sh components/redis.sh ;;
-  rabbitmq)   sh components/rabbitmq.sh ;;
-  mysql)      sh components/mysql.sh ;;
-  frontend)   sh components/frontend.sh ;;
+  cart)       CHECK $1; sh components/cart.sh ;;
+  catalogue)  CHECK $1; sh components/catalogue.sh ;;
+  user)       CHECK $1; sh components/user.sh ;;
+  shipping)   CHECK $1; sh components/shipping.sh ;;
+  payment)    CHECK $1; sh components/payment.sh ;;
+  mongodb)    CHECK $1; sh components/monogdb.sh ;;
+  redis)      CHECK $1; sh components/redis.sh ;;
+  rabbitmq)   CHECK $1; sh components/rabbitmq.sh ;;
+  mysql)      CHECK $1; sh components/mysql.sh ;;
+  frontend)   CHECK $1; sh components/frontend.sh ;;
   *)  echo -e "\e[1;31mUsage: Invalid Input\nInputs allowed are cart,catalogue,..........\e[0m"
       exit 1
       ;;
