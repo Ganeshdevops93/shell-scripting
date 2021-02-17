359a3cc006305a1f2984a45a7fd173fa9630a88e
KalluruS <>
2021-02-17 06:20:24 +0530

    Update shell-scripting/

--- roboshop/main.sh
+++ roboshop/main.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-sh components/$1.sh
+case $1 in
+  cart)       sh components/cart.sh ;;
+  catalogue)  sh components/catalogue.sh ;;
+  user)       sh components/user.sh ;;
+  shipping)   sh components/shipping.sh ;;
+  payment)    sh components/payment.sh ;;
+  mongodb)    sh components/monogdb.sh ;;
+  redis)      sh components/redis.sh ;;
+  rabbitmq)   sh components/rabbitmq.sh ;;
+  mysql)      sh components/mysql.sh ;;
+  frontend)   sh components/frontend.sh ;;
+  *)  echo -e "Usage: Invalid Input\eInputs allowed are cart,catalogue,.........."
+      exit 1
+      ;;
+esac
+
+      
