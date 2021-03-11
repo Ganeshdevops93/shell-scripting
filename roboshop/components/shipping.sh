979761a86e3b74a51a627fdfb414bbb68092a773
KalluruS <>
2021-03-11 15:35:30 +0530

    Update shell-scripting/

--- roboshop/components/shipping.sh
+++ roboshop/components/shipping.sh
@@ -12,13 +12,13 @@ Print "Adding RoboShop Project User" "useradd roboshop"
 id roboshop || useradd roboshop
 Stat $?
 
-Print "Download Shipping COde" 'curl -s -L -o /tmp/shipping.zip "https://dev.azure.com/DevOps-Batches/f635c088-1047-40e8-8c29-2e3b05a38010/_apis/git/repositories/9c06b317-6353-43f6-81e2-aa4f5f258b2d/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"'
+Print "Download Shipping COde" 'curl -s -L -o /tmp/shipping.zip "https://github.com/roboshop-devops-project/shipping/archive/main.zip"'
 
-curl -s -L -o /tmp/shipping.zip "https://dev.azure.com/DevOps-Batches/f635c088-1047-40e8-8c29-2e3b05a38010/_apis/git/repositories/9c06b317-6353-43f6-81e2-aa4f5f258b2d/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
+curl -s -L -o /tmp/shipping.zip "https://github.com/roboshop-devops-project/shipping/archive/main.zip"
 Stat $?
 
-Print "Extract Shipping COde" "mkdir /home/roboshop/shipping && cd /home/roboshop/shipping && unzip /tmp/shipping.zip"
-mkdir -p /home/roboshop/shipping && cd /home/roboshop/shipping && unzip -o /tmp/shipping.zip
+Print "Extract Shipping COde" "mkdir /home/roboshop/shipping && cd /home/roboshop && unzip /tmp/shipping.zip && mv shipping-main shipping && cd shipping"
+mkdir /home/roboshop/shipping && cd /home/roboshop && unzip /tmp/shipping.zip && mv shipping-main shipping && cd shipping
 Stat $?
 
 Print "Maven Compile Code" "mvn clean package && mv target/shipping-1.0.jar shipping.jar"
